ea64a640a52d1b1077b2a34b15a54346446fff06
mukeshrock7897 <mukeshrock7897@gmail.com>
2021-09-20 10:03:39 +0530

    second commit

--- test.py
+++ test.py
@@ -4,6 +4,13 @@ def JenkinsBuildTest():
         result[i]="test -"+str(i)
     return result
 
+def JenkinsBuildDeploy():
+    result={}
+    for i in range(10):
+        result[i]="deploy -"+str(i)
+    return result
+
 
 if __name__ == '__main__':
     print(JenkinsBuildTest())
+    print(JenkinsBuildDeploy())
